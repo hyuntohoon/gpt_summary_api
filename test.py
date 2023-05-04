11a7c93ef555e5624df94d61726968a603997c62
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-05-04 17:17:40 +0900

    refactor : delete test, move main

--- test.py
+++
@@ -1,6 +0,0 @@
-import openai
-
-openai.api_key = "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"
-
-completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Hello world!"}])
-print(completion.choices[0].message)
