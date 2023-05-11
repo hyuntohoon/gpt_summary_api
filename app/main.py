88ebabca60c8ff319c3909b66700a12597260deb
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-05-11 14:54:35 +0900

    refactor : main 함수 통합

--- app/main.py
+++ app/main.py
@@ -5,28 +5,14 @@ from routers import summary
 import openai
 
 
-def unicode_escape_to_text(escaped_str: str) -> str:
-    return escaped_str.encode('utf-8').decode('unicode_escape')
+class Settings(BaseSettings):
+    OPENAI_API_KEY: str = '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'
 
 
-def main():
-    class Settings(BaseSettings):
-        OPENAI_API_KEY: str = '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'
-        class Config:
-            env_file = '.env'
-
-    settings = Settings()
-    openai.api_key = settings.OPENAI_API_KEY
-
-    app = FastAPI()
-    app.include_router(summary.router)
-    return app
-
+settings = Settings()
+app = FastAPI()
+openai.api_key = settings.OPENAI_API_KEY
+app.include_router(summary.router)
 
 if __name__ == "__main__":
-    app = main()
-    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
-else:
-    app = main()
-
-
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info")
