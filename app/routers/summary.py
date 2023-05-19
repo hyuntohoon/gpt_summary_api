cd4c38f1f2ca45759625c07e5b2521c3dddcac02
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-05-19 11:42:59 +0900

    feat : GPT3.5 turbo 모델 추가

--- app/routers/summary.py
+++ app/routers/summary.py
@@ -16,18 +16,35 @@ class SummarizeInput(BaseModel):
 router = APIRouter()
 
 
-async def generate_summary(text: str, max_length: int = 1000):
+async def generate_summary_davinci(text: str, max_length: int = 500):
+    prompt = f"Could you please summarize the following sentence in Korean?\n\n{text}\n"
+
+    completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {
+            "role": "user",
+            "content": prompt,
+        }
+    ],
+    )
+
+    summary = completion.choices[0].message["content"]
+    return summary
+
+
+async def generate_summary_gpt3(text: str, max_length: int = 1000):
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt= f"Could you please summarize the following sentence in Korean?\n\n{text}\n",
         max_tokens=2000,
-        temperature=0.7,
+        temperature=1,
     )
     summary = response.choices[0].text.strip()
     return summary
 
 
-@router.post("/summarize_large_text")
+@router.post("/summarize_large_text_davinci_003")
 async def summarize_large_text(input_data: SummarizeInput):
     text = input_data.text # 텍스트 가져오기 리스트
     max_summarize_chars = input_data.max_summarize_chars
@@ -41,7 +58,28 @@ async def summarize_large_text(input_data: SummarizeInput):
         wrapped_text = wrapped_text[:length]
         summary_chunks = []
         for sub_chunk in enumerate(wrapped_text):
-            summarized_text = await generate_summary(sub_chunk, summary_length)
+            summarized_text = await generate_summary_gpt3(sub_chunk, summary_length)
+            summary_chunks.append(summarized_text)
+        final_summary_texts.append(summary_chunks)
+
+    return {"summary": final_summary_texts}
+
+
+@router.post("/summarize_large_text_GPT3.5_Turbo")
+async def summarize_large_text(input_data: SummarizeInput):
+    text = input_data.text # 텍스트 가져오기 리스트
+    max_summarize_chars = input_data.max_summarize_chars
+    max_chars_per_request = input_data.max_chars_per_request
+    summary_length = input_data.summary_length
+    final_summary_texts = []
+
+    for text_chunk in text:
+        wrapped_text = textwrap.wrap(text_chunk, max_chars_per_request)
+        length = max_summarize_chars // max_chars_per_request
+        wrapped_text = wrapped_text[:length]
+        summary_chunks = []
+        for sub_chunk in enumerate(wrapped_text):
+            summarized_text = await generate_summary_davinci(sub_chunk, summary_length)
             summary_chunks.append(summarized_text)
         final_summary_texts.append(summary_chunks)
 
